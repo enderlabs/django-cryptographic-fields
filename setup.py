804f9bb75940160bd9beb5d14bb1bb153207497f
Nevil Kothari <Nevil.Kothari@eptura.com>
2026-01-18 02:40:08 +0530

    TE-38809: Upgrade django-cryptographic-fields for compatibility with Django 5.2.10 and Python 3.10.16

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ setup(
     download_url='https://github.com/foundertherapy/django-cryptographic-fields/archive/' + version + '.tar.gz',
     author='Dana Spiegel',
     author_email='nasief304@gmail.com',
+    python_requires='>=3.10',
     install_requires=[
-        'Django>=1.7',
-        'cryptography>=0.8.2',
-        'six>=1.12.0',
+        'Django>=5.2',
+        'cryptography>=42.0.0',
     ],
     keywords=['encryption', 'django', 'fields', ],
 )
